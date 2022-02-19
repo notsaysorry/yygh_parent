6f2f40a9e7db2f5c8028c5904ebf6242baff1f72
liudong <1062659083@qq.com>
2022-02-20 01:27:03 +0800

    新增hospital-manage模块

---
+++ service/service_hosp/src/main/java/com/atguigu/yygh/hosp/service/impl/HospitalSetServiceImpl.java
@@ -0,0 +1,26 @@
+package com.atguigu.yygh.hosp.service.impl;
+
+
+import com.atguigu.yygh.hosp.mapper.HospitalSetMapper;
+import com.atguigu.yygh.hosp.service.HospitalSetService;
+import com.atguigu.yygh.model.hosp.HospitalSet;
+import com.atguigu.yygh.vo.order.SignInfoVo;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+
+
+@Service
+public class HospitalSetServiceImpl extends ServiceImpl<HospitalSetMapper, HospitalSet> implements HospitalSetService {
+
+
+    @Override
+    public String getSignKey(String hoscode) {
+        return null;
+    }
+
+    @Override
+    public SignInfoVo getSignInfoVo(String hoscode) {
+        return null;
+    }
+}
